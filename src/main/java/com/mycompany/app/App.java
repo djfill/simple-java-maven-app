f40db35e9c13cea8d403dba105e35c6b476e6660
Phil Evans <pevans@cafex.com>
2018-02-25 20:01:37 +0000

    Come up with a better app...

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Come on...come up with a better app!!!!!";
 
     public App() {}
 
